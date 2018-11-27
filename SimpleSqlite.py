8bc49c4bc61c38eab7ae5249818521b13e3ce203
Catch-XXII <cgultekink@gmail.com>
2018-11-27 11:33:39 +0300

    Create SimpleSqlite.py

---
+++ SimpleSqlite.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+name = input('Please enter your name: ')
+age = int(input('Please enter your age: '))
+email = input('Please enter your email: ')
+phone = input('Please enter your phone number: ')
+
+
+connection = sqlite3.connect('playerdb.db')
+
+cursor = connection.cursor()
+
+'''For the first time you have to uncomment below section in order to create a table with following properties '''
+# cursor.execute('create table player(id integer primary key, name text not null, '
+#                'age integer not null, email text unique not null, phone text unique not null);')
+
+'''After creation of table you have to comment out the code section above again if you are using PyCharm 
+the key combination is for ctrl + /
+'''
+
+cursor.execute('insert into player (name, age, email, phone) values(?, ?, ?, ?)', (name, age, email, phone))
+connection.commit()
+
+
+cursor.execute('select * from player')
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+connection.close()
+
+
+'''
+Two things are important here first don't forget the commit after insert and the second one is don't forget the
+close connection after select. in the first if you don't commit after insertion you will lose all your data.
+in the second statement if you forget the close the connection it will suck your memory maybe not in this 
+particular example obviously we don't have too much data which needs to be stored in the memory but if you have a huge
+database an open connection will drain your memory. 
+So, 
+1 => commit after insertion
+2 => close connection after select
+'''
